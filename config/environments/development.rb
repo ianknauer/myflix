233e715cec6cb9b1fc0b389b91af1021626c4315
ianknauer <ian.knauer@gmail.com>
2014-02-02 22:23:39 -0800

    updated config file for mailgun

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,4 +20,15 @@ Myflix::Application.configure do
   config.assets.debug = true
 
   config.eager_load = false
+  
+  ActionMailer::Base.smtp_settings = {
+     :port           => ENV['MAILGUN_SMTP_PORT'],
+     :address        => ENV['MAILGUN_SMTP_SERVER'],
+     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+     :domain         => 'sheltered-badlands-1944.heroku.com',
+     :authentication => :plain,
+   }
+
+   ActionMailer::Base.delivery_method = :smtp
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -16,4 +16,15 @@ Myflix::Application.configure do
 
   config.action_mailer.delivery_method = :test
   config.active_support.deprecation = :stderr
+  
+  ActionMailer::Base.smtp_settings = {
+     :port           => ENV['MAILGUN_SMTP_PORT'],
+     :address        => ENV['MAILGUN_SMTP_SERVER'],
+     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+     :domain         => 'sheltered-badlands-1944.heroku.com',
+     :authentication => :plain,
+   }
+
+   ActionMailer::Base.delivery_method = :smtp
 end
